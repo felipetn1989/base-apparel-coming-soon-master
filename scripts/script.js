66e7641838a2a9b747af9256e87aab0dc1825d0b
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-06 14:44:02 -0300

    added regex validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,20 @@
-function button() {
-  if (ifmail.value.length == 0) {
+submit.addEventListener("click", button);
+
+function validate(email) {
+  var mailformat =
+    /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+  if (email.value.match(mailformat)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function button(a) {
+  a.preventDefault();
+
+  if (ifmail.value.length == 0 || validate(ifmail) == false) {
     ifmail.style.borderColor = "var(--Soft-Red)";
     error.style.display = "block";
     error_msg.style.display = "block";
