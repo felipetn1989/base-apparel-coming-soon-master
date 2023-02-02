f5d27d4314a912ed54cbb4ce99766068aa180ba9
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-02-02 08:46:11 -0300

    finished update

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,25 @@
 submit.addEventListener("click", button);
 
-function validate(email) {
-  var mailformat =
-    /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-
-  if (email.value.match(mailformat)) {
-    return true;
-  } else {
-    return false;
-  }
-}
+let mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
 function button(x) {
   x.preventDefault();
 
-  if (ifmail.value.length == 0 || validate(ifmail) == false) {
-    ifmail.style.borderColor = "var(--Soft-Red)";
-    error.style.display = "block";
-    error_msg.style.display = "block";
+  ifmail.classList.toggle(
+    "color_change",
+    ifmail.value.length == 0 || !mailformat.test(ifmail.value)
+  );
+  error.classList.toggle(
+    "display",
+    ifmail.value.length == 0 || !mailformat.test(ifmail.value)
+  );
+  errorMsg.classList.toggle(
+    "display",
+    ifmail.value.length == 0 || !mailformat.test(ifmail.value)
+  );
+
+  if (ifmail.value.length == 0 || !mailformat.test(ifmail.value)) {
+    ifmail.value = " ";
     ifmail.focus();
   }
 }
